e67778afe6f086c14bc845e0ea758ee7a18e92a4
muhamadalfarisy <alfarisy.muhamad@tokopedia.com>
2022-12-15 23:23:45 +0700

    test

--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package go_say_hello
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestSayHello(t *testing.T) {
 	type args struct {
@@ -24,6 +26,31 @@ func TestSayHello(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := SayHello(tt.args.nama); got != tt.want {
 				t.Errorf("SayHello() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAddOne(t *testing.T) {
+	type args struct {
+		num int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "test",
+			args: args{num: 1},
+			want: 2,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := AddOne(tt.args.num); got != tt.want {
+				t.Errorf("AddOne() = %v, want %v", got, tt.want)
 			}
 		})
 	}
